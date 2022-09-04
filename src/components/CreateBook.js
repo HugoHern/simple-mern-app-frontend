6de044110913cc2f03e5bd49538658bdd8ecc99e
HugoHern <hugo8954@gmail.com>
2022-09-04 18:17:11 -0400

    updated router code to V6

---
+++ src/components/CreateBook.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom';
+import '../App.css';
+import axios from 'axios';
+
+function CreateBook() {
+  return (
+    <div>CreateBook</div>
+  )
+}
+
+export default CreateBook
